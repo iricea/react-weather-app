9e1d95d595ecad15de5bbc74a1c457bf7b39be71
Irina Liashenko <iricelwo@gmail.com>
2022-10-28 15:02:27 +0200

    added link to github repo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project is coded by Iryna Liashenko and is{" "}
+        <a
+          href="https://github.com/iricea/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-
