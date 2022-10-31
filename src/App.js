e37e38b33caf75305fe2ea4e96948e2939ab65e5
Irina Liashenko <iricelwo@gmail.com>
2022-10-31 15:09:37 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project is coded by Iryna Liashenko and is{" "}
-        <a
-          href="https://github.com/iricea/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project is coded by Iryna Liashenko and is{" "}
+          <a
+            href="https://github.com/iricea/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
