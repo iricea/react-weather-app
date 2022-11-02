6cd70ec42b9c1b5256a8a3ad7efc8c46e8aafc82
Irina Liashenko <iricelwo@gmail.com>
2022-11-02 11:41:36 +0100

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather app</h1>
+        <Weather />
         <footer>
           This project is coded by Iryna Liashenko and is{" "}
           <a
